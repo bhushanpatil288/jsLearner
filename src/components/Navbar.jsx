dd80063ff14a3c1411ad46b21969c2a765687703
bhushanpatil288 <sharewithbhushan@gmail.com>
2025-11-05 11:03:53 +0530

    [changes] - navbar-full width, responsive(true), [add] - hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,31 @@
+import { useState } from 'react'
+import { Menu } from '../constants'
 const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
-    <div className='container mx-auto border ng-red-500 flex justify-between px-5 bg-neutral-200 py-4 rounded-b-lg relative top-0 sticky'>
+    <div className='mx-auto ng-red-500 flex justify-between px-5 bg-neutral-200 py-4 relative top-0 sticky'>
       <div>Logo</div>
-      <ul className='flex gap-5'>
+      <ul className='flex gap-5 hidden md:flex'>
         <li>Home</li>
         <li>Contact</li>
         <li>About</li>
       </ul>
+      <div 
+        onClick={()=>(
+          setMenuOpen(!menuOpen)
+        )}
+        className='md:hidden relative'>
+        <Menu />
+        { menuOpen && (
+        <ul className='absolute bg-neutral-300 p-5 top-14 fixed right-0 z-20 w-full'>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About</li>
+        </ul>
+        )}
+      </div>
     </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
+// lucide icons
+import { Menu } from 'lucide-react';
+export { Menu }
+
+// local images
 import jsLogo from '../assets/jsLogo.png'
 
 export default jsLogo
